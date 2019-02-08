0001e250d3f22e56ba9484ee35849610fd77b98c
Rakib Uddin <16103030@iubat.edu>
2019-02-08 13:00:08 +0600

    Function and methods test

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Node-Practice
-Practicing Node Js
+Practicing Node Js and Also Documenting How I Do It. So if anyone in future wants to follow it they can :)
 
-Write in Console [node "fileName"]
-Or you can also get into node by writting node in the console then write JavaScript
+# Write in Console [node "fileName"] to Execute The JavaScript File
+You can also get into node by writting node in the console then write JavaScript
+
+# Clone The Code
+Everything inside is well documented. You can easily understand how they are working.
--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-console.log("Hello World");
+// // //Hello World
+// console.log("Hello World");
 
-setTimeout(() => {console.log("3sec")}, 3000)
+
+// // //setTimeout
+//setTimeout(() => {console.log("3sec")}, 3000)
+
+
+// // //setInterval
+// let i = 5;
+// setInterval(() => {console.log(i++)} ,1000)
+
+
+// // //Directory
+// console.log(__dirname);
+
+
+// // //FileName
+// console.log(__filename);
+
+
+// // //Normal Function
+// function hi(){
+//     console.log("hi")
+// }
+// hi();
+
+
+// // //Normal Function Expression
+// let bye = function(){
+//     console.log("bye")
+// }
+// bye();
+
+
+// // //ES6 Function Expression
+// let bye = () =>{
+//     console.log("bye")
+// }
+// bye();
+
+
