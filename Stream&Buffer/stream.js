eceb39f09f4b7ec9fa6cf1833ea24f11b53b61fc
Rakib Uddin <16103030@iubat.edu>
2019-02-11 18:21:44 +0600

    Routing With NodeJS

--- Stream&Buffer/stream.js
+++ Stream&Buffer/stream.js
@@ -1,10 +1,64 @@
-let fs = require("fs")
+let fs = require("fs");
+let http = require("http");
 
-let readStream = fs.createReadStream("../Text/read.txt" , "utf8");
-let writeStream = fs.createWriteStream("../Text/writeStrea.txt" , "utf8");
+// //Reading And Writting Stream Buffer
+// let writeStream = fs.createWriteStream("../Text/writeStrea.txt" , "utf8");
+
+
+// //Pipe
+// readStream.pipe(writeStream)
 
 
 
-readStream.on("data" , (chunk) => {
-    writeStream.write(chunk)
+
+let server = http.createServer(function(req , res){
+    console.log("Requested Url IS: "+req.url)
+    if(req.url === "/home" || req.url === "/"){
+        res.writeHead(200, {
+            "Content-Type" : "text/html"
+            })
+            fs.createReadStream("../HTML/index.html").pipe(res)
+    }
+    else if(req.url === "/about"){
+        res.writeHead(200, {
+            "Content-Type" : "text/html"
+            })
+            fs.createReadStream("../HTML/about.html").pipe(res)
+    }
+    else if(req.url === "/contact"){
+        res.writeHead(200, {
+            "Content-Type" : "text/html"
+            })
+            fs.createReadStream("../HTML/contact.html").pipe(res)
+    }
+    else if(req.url === "/api/rakib"){
+        res.writeHead(200, {
+            "Content-Type" : "application/json"
+            })
+            fs.createReadStream("../JSON/json.json").pipe(res)
+    }
+    else{
+        res.writeHead(200 , {
+            "Content-Type" : "text/html"
+        })
+        fs.createReadStream("../HTML/notFound.html").pipe(res)
+    }
+
+    // //Creating Object
+    // let obj = {
+    //     name : "Rakib",
+    //     job : "Web Developer",
+    //     age : 23
+    // }
+
 })
+
+server.listen(8001 , "127.0.0.1");
+
+
+
+
+// //Manual Way
+// readStream.on("data" , (chunk) => {
+//     writeStream.write(chunk)
+// })
